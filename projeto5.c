b778b69026dab48bd11d7105cf09d389e47ca716
marcelo046 <marc3lo046@gmail.com>
2018-06-13 15:50:58 -0300

    criado estrutura da árvore

--- projeto5.c
+++ projeto5.c
@@ -5,3 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct TREE {
+  int value;
+  struct TREE *left;
+  struct TREE *right;
+}tree;
+
+#include "funcoes/todas.h"
+
+int main() {
+
+
+
+
+
+
+  return 0;
+}

6d0a49ae9d7203763b2e27acca7e7e8d35dd5c7c
marcelo046 <marc3lo046@gmail.com>
2018-06-13 15:43:21 -0300

    criado programa principal

---
+++ projeto5.c
@@ -0,0 +1,7 @@
+//Alunos: Fabiana Luiza Vasconcelos Pfeilsticker Ribas  16/0005736
+//        Joao Pedro Mota Jardim                        16/0031982
+//        Marcelo Araujo dos Santos                     16/0035481
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>

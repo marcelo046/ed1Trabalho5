550b5e806eee1a5bf0faa92811f0ca6d9e1487b7
marcelo046 <marc3lo046@gmail.com>
2018-06-18 21:49:30 -0300

    feito caso de remoção de nó folha

--- funcoes/removeValue.h
+++ funcoes/removeValue.h
@@ -1,11 +1,30 @@
 
 tree * removeValue(tree * arvore, int valor) {
+  tree *aux = arvore;
+  tree *ant;
 
-  if (arvore->value == valor) {
-    
+  // primeiro procuramos o valor
+  while(aux->value != valor && aux != NULL){
+    ant = aux;
+    if(valor > aux->value)
+      aux = aux->right;
+    else
+      aux = aux->left;
   }
-  else {
+  // caso nao tiver encontrado o valor
+  if (aux == NULL) {
+    printf("valor %d nao foi encontrado!\n", valor);
+    pausar();
+    return arvore;
+  }
+  // caso o No nao tiver filho
+  if ((aux->right == NULL) && (aux->left == NULL)) {
+    if(valor > ant->value)
+      ant->right = NULL;
+    else
+      ant->left = NULL;
+    free(aux);
+  }
 
-  }
   return arvore;
 }

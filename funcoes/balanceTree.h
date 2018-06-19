26561efdd20edb74ddf6634ae58f3db530992f1f
marcelo046 <marc3lo046@gmail.com>
2018-06-19 01:34:59 -0300

    corrigindo erros

--- funcoes/balanceTree.h
+++ funcoes/balanceTree.h
@@ -9,12 +9,12 @@ void balanceTree(tree* arvore)
      return y;
   }
 //balance pera esquerda
-  struct tree* no;
-  no = (*arvore)->left;
-  (*arvore)->left = no->right;
-  no->right = *arvore;
-  (*arvore)->altura = maior(altura((*arvore)->left), altura((*arvore)->right))+1;
-  no->altura = maior(altura(no->left), (*arvore)->altura+1;
-  *arvore = no;
+  tree* no;
+  no = arvore->left;
+  arvore->left = no->right;
+  no->right = arvore;
+  arvore->altura = maior(altura(arvore->left), altura(arvore->right))+1;
+  no->altura = maior(altura(no->left), arvore->altura+1;
+  arvore = no;
 
 }
